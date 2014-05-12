350e67aff0864e3de18f7d521d7cf9dd82462f76
shunsaka <shunsaka@users.noreply.github.com>
2014-05-13 00:48:52 +0900

    first commit

---
+++ shunsaka_mobile_eastasia/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using shunsaka_mobile_eastasia.Resources;
+
+namespace shunsaka_mobile_eastasia
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
